df57c085b408be772718943077adc4efbb4da992
lipan <lipan16@lzu.edu.cn>
2023-11-18 15:55:02 +0800

    feat: SVG组件

---
+++ src/components/SvgIcon.js
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+
+const importAll = (requireContext) => requireContext.keys().forEach(requireContext)
+
+try{
+    console.log('import all assets of svg')
+    importAll(require.context('@/assets/svg', false, /\.svg$/))
+}catch(error){
+    console.error('import all svg ERROR', error.message)
+}
+
+const SvgIcon = ({width, height, color, className, style, onClick, filename}) => {
+    return (
+        <svg width={width || 10}
+             height={height || 18}
+             fill={color}
+             className={className}
+             style={style}
+             onClick={onClick}
+        >
+            <use xlinkHref={'#' + filename}/>
+        </svg>
+    )
+}
+SvgIcon.propTypes = {
+    width: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    color: PropTypes.string,
+    className: PropTypes.string,
+    style: PropTypes.object,
+    onClick: PropTypes.func,
+    filename: PropTypes.string.isRequired
+}
+export default SvgIcon
