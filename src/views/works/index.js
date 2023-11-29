4bdf8d3d2966a059ae0e7f1d5ff8579d727deb8b
lipan16 <lipan16@lzu.edu.cn>
2023-11-29 20:09:00 +0800

    feat: 作品页面

--- src/views/works/index.js
+++ src/views/works/index.js
@@ -1,12 +1,39 @@
 import {useTitle} from 'ahooks'
+import {Flex} from 'antd'
 import React from 'react'
+
+import './index.less'
 
 const Works = () => {
     useTitle('作品')
 
+    const workList = [
+        {link: '/', bgImage: 'http://8.133.162.30/static/asi.png', title: '个人主页', desc: '记录个人日常'},
+        {link: '/web-knowledge', bgImage: 'http://8.133.162.30/static/web_knowledge.png', title: 'web 前端知识', desc: '日常学习、面试的前端知识在线库'},
+        {
+            link: 'https://pvp.qq.com/web201605/wallpaper.shtml',
+            bgImage: 'http://8.133.162.30/static/2735011818.jpg',
+            title: '王者荣耀',
+            desc: '王者荣耀游戏壁纸'
+        }
+    ]
+
     return (
         <section className='works'>
-            <h2>作品</h2>
+            <h1>作品</h1>
+            <Flex wrap='wrap' gap='2.5rem' className='works-content'>
+                {workList.map(work =>
+                    <div
+                        key={work.link}
+                        className='works-item'
+                        onClick={() => window.open(work.link)}
+                        style={{backgroundImage: `url(${work.bgImage})`}}>
+
+                        <div className='title'>{work.title}</div>
+                        <div className='desc'>{work.desc || work.title}</div>
+                    </div>
+                )}
+            </Flex>
         </section>
     )
 }
