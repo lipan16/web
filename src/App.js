921671d0663d163941796fea0c81bec1eada1b35
lipan16 <lipan16@lzu.edu.cn>
2023-12-02 16:23:55 +0800

    feat: 菜单栏添加暗色模式

--- src/App.js
+++ src/App.js
@@ -3,21 +3,40 @@ import {useSelector} from 'react-redux'
 import {ConfigProvider, theme} from 'antd'
 import {StyleProvider, px2remTransformer} from '@ant-design/cssinjs'
 
-import SelfLayout from '@/components/layout'
-// import theme from '@/theme'
+const {useToken} = theme
 import '@/utils/event'
-import './global.less'
-import '@/theme/antd.less'
+import SelfLayout from '@/components/layout'
+import '@/global.less'
+import '@/antd.less'
 
 const px2rem = px2remTransformer({
     rootValue: 16 // 根元素字体大小 rootValue px = 1rem
 })
 
 const App = () => {
+    const {token} = useToken()
     const themeStore = useSelector(state => state.setting.theme)
 
     const themeProvider = useMemo(() => {
-        return {token: themeStore.token, algorithm: themeStore.dark ? theme.darkAlgorithm : theme.defaultAlgorithm}
+        return {
+            token: themeStore.token,
+            algorithm: themeStore.dark ? theme.darkAlgorithm : theme.defaultAlgorithm,
+            components: {
+                Menu: {
+                    itemBg: 'transparent',
+                    itemHoverColor: themeStore.token.colorPrimary,
+                    itemSelectedColor: themeStore.token.colorPrimary,
+                    activeBarBorderWidth: 0,
+                    activeBarHeight: 0,
+                    itemPaddingInline: 12,
+                    iconMarginInlineEnd: 8,
+                },
+                FloatButton: {
+                    colorBgElevated: themeStore.token.colorPrimary,
+                    colorText: token.colorWhite
+                }
+            }
+        }
     }, [themeStore])
 
     return (
