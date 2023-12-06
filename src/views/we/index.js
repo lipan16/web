d6a867c4771d8fc509c4c4510faa1a56434b223d
lipan16 <lipan16@lzu.edu.cn>
2023-12-06 21:44:43 +0800

    feat: 我们页面完善

--- src/views/we/index.js
+++ src/views/we/index.js
@@ -1,11 +1,12 @@
-import dayjs from 'dayjs'
 import React, {useCallback, useState} from 'react'
 import {Outlet, useNavigate} from 'react-router-dom'
-import {Button} from 'antd'
+import dayjs from 'dayjs'
 import {useTitle, useInterval} from 'ahooks'
+import {Button} from 'antd'
 
 import {BPI_TIME} from '@/constants'
 import {showTime} from '@/utils'
+import './index.less'
 
 const We = () => {
     useTitle('We')
@@ -14,17 +15,20 @@ const We = () => {
     const [bpiTime, setBpiTime] = useState('')
 
     useInterval(() => {
-        setBpiTime(showTime(BPI_TIME))
+        const time = showTime(BPI_TIME)
+        setBpiTime(time.replace(/(\d+)/g, `<strong class='time'>$1</strong>`))
     }, 1000, {immediate: true})
 
     const onClickBtn = useCallback((path) => {
         navigate(path)
     }, [])
 
-
     return (
-        <section className='lipan'>
-            <h1>We: {bpiTime}</h1>
+        <section className='we' style={{backgroundImage: 'url(http://8.133.162.30/static/2735011818111.jpg)'}}>
+            <div className='love-time'>
+                <div className='title'>这是我们相爱的</div>
+                <div dangerouslySetInnerHTML={{__html: '第' + bpiTime}}/>
+            </div>
             <h2>来世界的1万天是：{dayjs('1998-12-17').add(10000, 'days').format('YYYY-MM-DD')}</h2>
             <h2>来世界的1万天是：{dayjs('1999-06-20').add(10000, 'days').format('YYYY-MM-DD')}</h2>
             <Button onClick={() => onClickBtn('lipan')}>lipan</Button>
