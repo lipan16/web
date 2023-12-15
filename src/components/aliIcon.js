629d1b745e023a4dd589e016c4f29017307b033b
lipan16 <lipan16@lzu.edu.cn>
2023-12-15 19:28:50 +0800

    feat: 增加图标

--- src/components/aliIcon.js
+++ src/components/aliIcon.js
@@ -1,7 +1,7 @@
 import {createFromIconfontCN} from '@ant-design/icons'
 
 const IconFont = createFromIconfontCN({
-    scriptUrl: ['//at.alicdn.com/t/c/font_4338783_thftfx7a6t.js']
+    scriptUrl: ['//at.alicdn.com/t/c/font_4338783_ildqwge6w7.js']
 })
 
 export default IconFont
