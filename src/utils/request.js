d95dcc661bbd7670266e5df6c7552ddde5d3a721
lipan16 <lipan16@lzu.edu.cn>
2023-12-24 18:02:06 +0800

    feat: request优化

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,11 +1,13 @@
 function fetchRequest({url, method = 'POST', data, loading = false, loadingContent = '加载中...'}){
     let body = ''
-    for(let key in data){
-        body += key + '=' + data[key] + '&'
-    }
 
     if(['get', 'GET', 'head', 'HEAD'].includes(method)){
-        url += '?' + body.slice(0, -1)
+        for(let key in data){
+            body += `${key}=${data[key]}&`
+        }
+        if(body){
+            url += '?' + body.slice(0, -1)
+        }
         body = null
     }else{
         body = data
@@ -21,6 +23,7 @@ function fetchRequest({url, method = 'POST', data, loading = false, loadingConte
             'Accept': 'application/json;*/*;',
             'Cache-Control': 'no-cache'
         },
+        mode: 'cors',
         body // get 或者head 提交的请求不能有body
     }).then(res => res.json())
 }
