7f2e692c8fd7bb1c36fefa79bfc3422b059bc459
lipan16 <lipan16@lzu.edu.cn>
2023-12-30 18:41:58 +0800

    feat: dayjs引入isToday插件

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ import dayjs from 'dayjs'
 import 'dayjs/locale/zh-cn' // 导入本地化语言
 import * as duration from 'dayjs/plugin/duration' // 导入插件
 import dayOfYear from 'dayjs/plugin/dayOfYear'
+import isToday from 'dayjs/plugin/isToday'
 
 dayjs.locale('zh-cn') // 使用本地化语言
 dayjs.extend(duration) // 使用插件
 dayjs.extend(dayOfYear)
+dayjs.extend(isToday)
 
 import store from '@/store'
 import routes from '@/router'
