eb8a63e40aaa06821dad4997aef0a1008b8ac675
lipan16 <lipan16@lzu.edu.cn>
2024-06-12 21:07:12 +0800

    feat: 并发请求，重试请求

--- src/utils/request.js
+++ src/utils/request.js
@@ -21,6 +21,7 @@ function createFetchWithTimeout(timeout = 5000){
                 mode: 'cors',
                 ...options,
             }).then(resolve, reject)
+
             setTimeout(() => {
                 reject(new Error('请求超时'))
                 controller.abort() // 终止请求
@@ -47,6 +48,53 @@ function fetchRequest({url, method = 'POST', data, loading = false, loadingConte
     }
 
     return fetchWithTimeout(url, {method, headers, body}).then(res => res.json())
+}
+
+/**
+ * 并发请求
+ * @param urls 请求地址数组
+ * @param maxNum 最大并发数
+ */
+const fetchRequestMax = (urls, maxNum = 5) => {
+    if(urls.length === 0) return Promise.resolve([])
+
+    return new Promise(resolve => {
+        const results = [] // 请求结果数组
+        let count = 0 // 已完成的请求数
+        let index = 0 // 当前请求的索引
+
+        async function _fetchRequest(){
+            const i = index
+            const url = urls[i]
+            index++
+            try{
+                results[i] = await fetchRequest({url})
+            }catch(err){
+                results[i] = err
+            }finally{
+                count++
+                if(count === urls.length){
+                    resolve(results)
+                }
+                if(index < urls.length){
+                    _fetchRequest()
+                }
+            }
+        }
+
+        for(let i = 0; i < Math.min(urls.length, maxNum); i++){
+            _fetchRequest()
+        }
+    })
+}
+
+/**
+ * 请求重试
+ * @param url 请求地址
+ * @param retryNum 重试次数
+ */
+const fetchRequestRetry = (url, retryNum = 3) => {
+    return fetchRequest({url}).catch(err =>retryNum <= 0 ? Promise.reject(err) : fetchRequestRetry(url, retryNum - 1))
 }
 
 export default fetchRequest
